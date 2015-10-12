606189f999a13a671594aec9bdfca33a0c3179b5
Casey Lakritz <caseylakritz@caseys-mbp.home>
2015-10-12 15:42:36 -0400

    server up and welcome page created

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
-var express = require('express'),
-	PORT = process.env.PORT || 5432,
-	server = express(),
-	MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
-	dbname = "Same Name",
-	mongoose = require('mongoose');
+var   express           = require('express'),
+	    PORT              = process.env.PORT || 3002,
+	    server            = express(),
+	    MONGOURI          = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
+	    dbname            = "same_name",
+	    mongoose          = require('mongoose'),
+      bodyParser        = require('body-parser'),
+      ejs               = require('ejs'),
+      expressEjsLayouts = require('express-ejs-layouts'),
+      methodOverride    = require('method-override');
+      // morgan            = require('morgan');
+
+server.set('views', './views');
+server.set('view engine', 'ejs');
+
+// server.use(morgan('dev'));
+server.use(express.static('./public'));
+
+server.use(expressEjsLayouts);
 
 server.get('/', function(req, res) {
-	res.write("Welcome to my app");
-	res.end();
-})
+	res.render('welcome');
+});
 
-mongoose.connect(MONGOURI + '/' + dbname);
+mongoose.connect(MONGOURI + "/" + dbname);
 
 server.listen(PORT, function () {
 	console.log("Up", PORT);
