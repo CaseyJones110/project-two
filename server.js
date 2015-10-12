625a40c4e7f688854cb78661f3930bdfd7ab74ed
Casey Lakritz <caseylakritz@caseys-mbp.home>
2015-10-12 18:23:13 -0400

    new article submission form added

--- server.js
+++ server.js
@@ -4,22 +4,36 @@ var   express           = require('express'),
 	    MONGOURI          = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
 	    dbname            = "same_name",
 	    mongoose          = require('mongoose'),
+      Schema            = mongoose.Schema,
       bodyParser        = require('body-parser'),
       ejs               = require('ejs'),
       expressEjsLayouts = require('express-ejs-layouts'),
-      methodOverride    = require('method-override');
-      // morgan            = require('morgan');
+      methodOverride    = require('method-override'),
+      morgan            = require('morgan');
+
+var articleSchema = new Schema({
+  author: String,
+  categories: String,
+  body: String
+}, {collection: 'articles', strict: false});
+
+var article = mongoose.model('article', articleSchema);
 
 server.set('views', './views');
 server.set('view engine', 'ejs');
 
-// server.use(morgan('dev'));
+server.use(morgan('dev'));
 server.use(express.static('./public'));
-
+server.use(bodyParser.urlencoded({ extended: true}));
+server.use(methodOverride('_method'));
 server.use(expressEjsLayouts);
 
 server.get('/', function(req, res) {
 	res.render('welcome');
+});
+
+server.get('/new', function (req, res) {
+  res.render('new_article');
 });
 
 mongoose.connect(MONGOURI + "/" + dbname);
