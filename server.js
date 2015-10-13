3ea717e960058252b57569340a928277b25d395b
Casey Lakritz <caseylakritz@Caseys-MacBook-Pro.local>
2015-10-13 13:50:11 -0400

    articles can now be created and read

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var   express           = require('express'),
 	    PORT              = process.env.PORT || 3002,
 	    server            = express(),
 	    MONGOURI          = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
-	    dbname            = "same_name",
+	    dbname            = "wiki",
 	    mongoose          = require('mongoose'),
       Schema            = mongoose.Schema,
       bodyParser        = require('body-parser'),
@@ -12,12 +12,13 @@ var   express           = require('express'),
       morgan            = require('morgan');
 
 var articleSchema = new Schema({
+  title: String,
   author: String,
-  categories: String,
+  categories: [String],
   body: String
 }, {collection: 'articles', strict: false});
 
-var article = mongoose.model('article', articleSchema);
+var Article = mongoose.model('article', articleSchema);
 
 server.set('views', './views');
 server.set('view engine', 'ejs');
@@ -32,12 +33,50 @@ server.get('/', function(req, res) {
 	res.render('welcome');
 });
 
-server.get('/new', function (req, res) {
+server.get('/articles/new', function (req, res) {
   res.render('new_article');
+});
+
+server.post('/articles', function (req, res) {
+  var article = new Article (req.body.article);
+  article.save(function (err, article) {
+    if (err) {
+      res.redirect(302, '/articles/new');
+    } else {
+      res.redirect(302, '/articles');
+    }
+  });
+});
+
+server.get('/articles', function (req, res) {
+  Article.find({}, function (err, allArticles) {
+    if (err) {
+      console.log("ERROR ==" + err);
+    } else {
+      res.render('index', {
+        articles: allArticles
+      });
+    }
+  });
 });
 
 mongoose.connect(MONGOURI + "/" + dbname);
 
 server.listen(PORT, function () {
-	console.log("Up", PORT);
+  var newArticle = new Article ({
+    title: "Testing nodedemon",
+    author: "Casey",
+    categories: ['tag', 'no tag', 'some tags'],
+    body: "This is my super awesome and informative article"
+  })
+
+  newArticle.save(function(err, article) {
+    if (err) {
+      console.log("ERROR" + err);
+    } else {
+      console.log("article save", article);
+    }
+  })
+
+  console.log("Up", PORT);
 })
